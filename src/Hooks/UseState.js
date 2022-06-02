03896478d687610c7b72033e4f19b23c63480ea4
hafizip00 <hafizip00@gmail.com>
2022-06-02 13:36:23 -0700

    useState Hook

---
+++ src/Hooks/UseState.js
@@ -0,0 +1,38 @@
+import {React, useState} from 'react'
+
+function UseState() {
+
+const initial = {count : 0 , theme : 'Blue'};
+
+const run = ()=>{ // This will run everytime I render
+    console.log("Runner");
+}
+
+    const [count , setCount] = useState(0);
+    // Testing 0
+    const [oCount , setOcount] = useState(run) // This will run everytime I render
+    // Testing 1
+    const [countTwo , setCountTwo] = useState(()=>{ //  runs only once
+        console.log("Runner");
+    })
+
+
+
+
+  return (
+    <div>
+        <p>
+            Simple use state 
+            <h1>{count}</h1>
+        </p>
+        <button onClick={() => setCount(prev => prev+1)}>Increment</button>
+        <button onClick={() => setCount(prev => prev-1)}>Decrement</button>
+
+        <br/>
+
+
+    </div>
+  )
+}
+
+export default UseState
